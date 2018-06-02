bcb61136f2a03e24ab550eb4b7e57cdae39fc9a4
Miguel Angel Pari Rojas <miangel-pr@outlook.com>
2018-06-01 23:00:12 -0400

    Add basic project structure

---
+++ at_serenity_screenplay_junit/src/test/java/at/serenity/sample/tasks/OpenTheApplication.java
@@ -0,0 +1,19 @@
+package at.serenity.sample.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
+import at.serenity.sample.ui.GoogleSearchPage;
+
+public class OpenTheApplication implements Task {
+
+    GoogleSearchPage googleSearchPage;
+
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(googleSearchPage)
+        );
+    }
+}
